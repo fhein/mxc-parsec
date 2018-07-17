01d12430b188492291ecc886e3e841e23d6cfd3b
mxc-commons <frank.hein@maxence.de>
2018-07-17 15:08:36 +0200

    Fixed a bug which prevented the use of the alternative skipper provided to the skip directive.

--- src/Spirit/Qi/Directive/SkipDirective.php
+++ src/Spirit/Qi/Directive/SkipDirective.php
@@ -3,15 +3,26 @@
 namespace Mxc\Parsec\Qi\Directive;
 
 use Mxc\Parsec\Qi\UnusedSkipper;
+use Mxc\Parsec\Qi\Domain;
 use Mxc\Parsec\Qi\Parser;
 use Mxc\Parsec\Qi\DelegatingParser;
 
 class SkipDirective extends DelegatingParser
 {
+    public function __construct(Domain $domain, string $uid, $subject, $skipper = null)
+    {
+        parent::__construct($domain, $uid, $subject);
+
+        $this->skipper = $skipper;
+    }
+
     public function doParse($skipper)
     {
-        if (isset($this->args[0]) && ($this->args[0] instanceof Parser)) {
-            $skipper = $this->args[0];
+        if (is_string($this->skipper)) {
+            $this->skipper = $this->domain->getParser($this->skipper);
+        }
+        if ($this->skipper instanceof Parser) {
+            $skipper = $this->skipper;
         } elseif ($skipper instanceof UnusedSkipper) {
             $skipper = $skipper->getSkipper();
         }
